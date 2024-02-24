54c56cef21981e46cde9c268dc7b8110b1b63b6b
OMEN\ruben <ruben-carmona@outlook.com>
2024-02-24 21:24:05 +0100

    feat: Add model and update properties.

---
+++ src/main/java/com/rubencarmona/inditex/domain/Rate.java
@@ -0,0 +1,31 @@
+package com.rubencarmona.inditex.domain;
+
+import lombok.Builder;
+import lombok.Data;
+import java.time.Instant;
+
+/**
+ * The type Price.
+ */
+@Builder
+@Data
+public class Rate {
+
+    private Long id;
+
+    private Long brandId;
+
+    private Instant startDate;
+
+    private Instant endDate;
+
+    private Long priceList;
+
+    private Long productId;
+
+    private Integer priority;
+
+    private Double price;
+
+    private String curr;
+}
